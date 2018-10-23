6d2ff6812f76478a2c8416aa792896f80a52e54b
Naren <mknnaren@cloudmpower.com>
2018-10-23 18:14:37 +0530

    handle destroy event and remove unused code

--- src/android/MusicControlsBroadcastReceiver.java
+++ src/android/MusicControlsBroadcastReceiver.java
@@ -36,101 +36,18 @@ public class MusicControlsBroadcastReceiver extends BroadcastReceiver {
 		
 		if (this.cb != null){
 			String message = intent.getAction();
-			Bundle pextras = intent.getExtras();
-			int notiId = pextras.getInt("NOTIFICATION_ID");
-			String notiData = pextras.getString("NOTIFICATION_DATA");
-			if(message.equals(Intent.ACTION_HEADSET_PLUG)){
-				// Headphone plug/unplug
-				int state = intent.getIntExtra("state", -1);
-				switch (state) {
-					case 0:
-						this.cb.success("{\"message\": \"music-controls-headset-unplugged\"}");
-						this.cb = null;
-						this.musicControls.unregisterMediaButtonEvent();
-						break;
-					case 1:
-						this.cb.success("{\"message\": \"music-controls-headset-plugged\"}");
-						this.cb = null;
-						this.musicControls.registerMediaButtonEvent();
-						break;
-					default:
-						break;
-				}
-			} else if (message.equals("music-controls-media-button")){
-				// Media button
-				KeyEvent event = (KeyEvent) intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
-				if (event.getAction() == KeyEvent.ACTION_DOWN) {
+			int notiId = 0;
+			String notiData = "";
+			if (intent.hasExtra("NOTIFICATION_ID")) {
+				Bundle pextras = intent.getExtras();
+				notiId = pextras.getInt("NOTIFICATION_ID");
+				notiData = pextras.getString("NOTIFICATION_DATA");
+			}
 
-					int keyCode = event.getKeyCode();
-					switch (keyCode) {
-						case KeyEvent.KEYCODE_MEDIA_NEXT:
-							this.cb.success("{\"message\": \"music-controls-media-button-next\"}");
-							break;
-						case KeyEvent.KEYCODE_MEDIA_PAUSE:
-							this.cb.success("{\"message\": \"music-controls-media-button-pause\"}");
-							break;
-						case KeyEvent.KEYCODE_MEDIA_PLAY:
-							this.cb.success("{\"message\": \"music-controls-media-button-play\"}");
-							break;
-						case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
-							this.cb.success("{\"message\": \"music-controls-media-button-play-pause\"}");
-							break;
-						case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
-							this.cb.success("{\"message\": \"music-controls-media-button-previous\"}");
-							break;
-						case KeyEvent.KEYCODE_MEDIA_STOP:
-							this.cb.success("{\"message\": \"music-controls-media-button-stop\"}");
-							break;
-						case KeyEvent.KEYCODE_MEDIA_FAST_FORWARD:
-							this.cb.success("{\"message\": \"music-controls-media-button-fast-forward\"}");
-							break;
-						case KeyEvent.KEYCODE_MEDIA_REWIND:
-							this.cb.success("{\"message\": \"music-controls-media-button-rewind\"}");
-							break;
-						case KeyEvent.KEYCODE_MEDIA_SKIP_BACKWARD:
-							this.cb.success("{\"message\": \"music-controls-media-button-skip-backward\"}");
-							break;
-						case KeyEvent.KEYCODE_MEDIA_SKIP_FORWARD:
-							this.cb.success("{\"message\": \"music-controls-media-button-skip-forward\"}");
-							break;
-						case KeyEvent.KEYCODE_MEDIA_STEP_BACKWARD:
-							this.cb.success("{\"message\": \"music-controls-media-button-step-backward\"}");
-							break;
-						case KeyEvent.KEYCODE_MEDIA_STEP_FORWARD:
-							this.cb.success("{\"message\": \"music-controls-media-button-step-forward\"}");
-							break;
-						case KeyEvent.KEYCODE_META_LEFT:
-							this.cb.success("{\"message\": \"music-controls-media-button-meta-left\"}");
-							break;
-						case KeyEvent.KEYCODE_META_RIGHT:
-							this.cb.success("{\"message\": \"music-controls-media-button-meta-right\"}");
-							break;
-						case KeyEvent.KEYCODE_MUSIC:
-							this.cb.success("{\"message\": \"music-controls-media-button-music\"}");
-							break;
-						case KeyEvent.KEYCODE_VOLUME_UP:
-							this.cb.success("{\"message\": \"music-controls-media-button-volume-up\"}");
-							break;
-						case KeyEvent.KEYCODE_VOLUME_DOWN:
-							this.cb.success("{\"message\": \"music-controls-media-button-volume-down\"}");
-							break;
-						case KeyEvent.KEYCODE_VOLUME_MUTE:
-							this.cb.success("{\"message\": \"music-controls-media-button-volume-mute\"}");
-							break;
-						case KeyEvent.KEYCODE_HEADSETHOOK:
-							this.cb.success("{\"message\": \"music-controls-media-button-headset-hook\"}");
-							break;
-						default:
-							this.cb.success("{\"message\": \"" + message + "\",\"data\": \""+notiData+"\"}");
-							break;
-					}
-					this.cb = null;
-				}
-			} else if (message.equals("music-controls-destroy")){
+			if (message.equals("music-controls-destroy")){
 				// Close Button
 				this.cb.success("{\"message\": \"music-controls-destroy\", \"data\": \""+notiData+"\"}");
 				this.cb = null;
-				this.musicControls.destroyPlayerNotification();
 			} else {
 				this.cb.success("{\"message\": \"" + message + "\",\"data\": \""+notiData+"\"}");
 				this.musicControls.destroySinglePlayerNotification(notiId);
